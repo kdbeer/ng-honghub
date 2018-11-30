27cbfb7a773a321da4bbc38240a93a0756f5920f
Kridsada Duangmala <kridsadacpe@gmail.com>
2018-11-30 17:57:46 +0700

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {StoreModule} from '@ngrx/store';
-import {reducers, metaReducers} from './reducers';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '../environments/environment';
-import {EffectsModule} from '@ngrx/effects';
-import {AuthGuard} from './auth/guards/auth.guard'
-import {ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule, AppRoutingModule, ReactiveFormsModule,
-        EffectsModule.forRoot([]),
-        StoreModule.forRoot(reducers, {metaReducers}),
-        !environment.production
-            ? StoreDevtoolsModule.instrument()
-            : []
-    ],
-    providers: [AuthGuard],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
+  providers: [AuthGuard],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
