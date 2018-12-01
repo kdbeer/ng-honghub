6d7c3f98c5abcbe359ae1ec30dd3427888c412b5
Kridsada Duangmala <kridsadacpe@gmail.com>
2018-12-02 02:04:51 +0700

    add modal

---
+++ src/app/user/confirm-reserve-modal/confirm-reserve-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-reserve-modal',
+  templateUrl: './confirm-reserve-modal.component.html',
+  styleUrls: ['./confirm-reserve-modal.component.scss']
+})
+export class ConfirmReserveModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/user/reserve-able/reserve-able.component.ts
+++ src/app/user/reserve-able/reserve-able.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from 'app/models/page';
 
 @Component({
   selector: 'app-reserve-able',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reserve-able.component.scss']
 })
 export class ReserveAbleComponent implements OnInit {
+  columns = [
+    { name: '#', prop: 'id', maxWidth: 50 },
+    { name: 'Room', prop: 'room' },
+    { name: 'Start Time', prop: 'start_time' },
+    { name: 'End Time', prop: 'end_time' },
+    { name: 'Reserver', prop: 'reserver' }
+  ];
 
-  constructor() { }
+  rows: any;
+  page: Page;
+  selected = [];
 
-  ngOnInit() {
+  constructor() {
+    this.rows = [
+      { name: 'Austin', gender: 'Male', room: 'Swimlane' },
+      { name: 'Dany', gender: 'Male', room: 'KFC' },
+      { name: 'Molly', gender: 'Female', room: 'Burger King' }
+    ];
+    this.page = {
+      size: 100,
+      totalPages: 10,
+      pageNumber: 0,
+      totalElements: 10
+    };
   }
 
+  ngOnInit() {}
 }
