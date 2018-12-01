bb0634ecd31a4d0a5a1dd32eb85733d2b67e9e53
Kridsada Duangmala <kridsadacpe@gmail.com>
2018-12-01 09:24:43 +0700

    intial data table

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from '../models/page';
 
 @Component({
   selector: 'app-rooms',
@@ -6,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.scss']
 })
 export class RoomsComponent implements OnInit {
-  constructor() {}
+  columns = [
+    { name: '#', prop: 'id', maxWidth: 50 },
+    { name: 'Room', prop: 'room' },
+    { name: 'Start Time', prop: 'start_time' },
+    { name: 'End Time', prop: 'end_time' },
+    { name: 'Reserver', prop: 'reserver' }
+  ];
+
+  rows: any;
+  page: Page;
+
+  constructor() {
+    this.rows = [
+      { name: 'Austin', gender: 'Male', room: 'Swimlane' },
+      { name: 'Dany', gender: 'Male', room: 'KFC' },
+      { name: 'Molly', gender: 'Female', room: 'Burger King' }
+    ];
+    this.page = {
+      size: 100,
+      totalPages: 10,
+      pageNumber: 0,
+      totalElements: 10
+    };
+  }
 
   ngOnInit() {}
+
+  setPage(e) {
+    console.log(e);
+  }
 }
--- src/app/rooms/rooms.module.ts
+++ src/app/rooms/rooms.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RoomsRoutingModule } from './rooms-routing.module';
 import { RoomsComponent } from './rooms.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 @NgModule({
   declarations: [RoomsComponent],
-  imports: [
-    CommonModule,
-    RoomsRoutingModule
-  ]
+  imports: [CommonModule, NgxDatatableModule, RoomsRoutingModule]
 })
-export class RoomsModule { }
+export class RoomsModule {}
