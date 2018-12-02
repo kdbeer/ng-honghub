27e55a6c023d31cc8c6012cbb96db448be8eca34
Kridsada Duangmala <kridsadacpe@gmail.com>
2018-12-02 10:27:55 +0700

    save

--- src/app/user/reserve.reducer.ts
+++ src/app/user/reserve.reducer.ts
@@ -1,19 +1,46 @@
 import { Action } from '@ngrx/store';
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { ReserveActionTypes, ReserveActions } from './reserve.actions';
+import { stat } from 'fs';
 
-export interface ReserveState {
+export interface AvalibleRoom {
+  name: string;
+  building: string;
+  type: string;
+  capacity: number;
+}
+
+export interface ReserveState extends EntityState<AvalibleRoom> {
   open_modal: boolean;
   confirm_reserve_modal: boolean;
   form: any;
-  available: any[];
+  available: any;
 }
 
-export const initialState: ReserveState = {
+export const adapter: EntityAdapter<AvalibleRoom> = createEntityAdapter<
+  AvalibleRoom
+>();
+
+export const initialState: ReserveState = adapter.getInitialState({
   open_modal: false,
   confirm_reserve_modal: false,
   form: undefined,
   available: undefined
-};
+});
+
+// export interface ReserveState {
+//   open_modal: boolean;
+//   confirm_reserve_modal: boolean;
+//   form: any;
+//   available: any[];
+// }
+
+// export const initialState: ReserveState = {
+//   open_modal: false,
+//   confirm_reserve_modal: false,
+//   form: undefined,
+//   available: undefined
+// };
 
 export function reducer(
   state = initialState,
@@ -23,20 +50,26 @@ export function reducer(
     case ReserveActionTypes.OpenSearchModal:
       return { ...state, open_modal: true };
     case ReserveActionTypes.CloseSearchModal:
-      return { ...state, open_modal: false };
+      return { ...state, open_modal: false, confirm_reserve_modal: false };
     case ReserveActionTypes.ConfirmSearchAvailable:
       return { ...state, open_modal: false };
     case ReserveActionTypes.SearchAvailableFailed:
       return { ...state, open_modal: false };
     case ReserveActionTypes.SearchAvailableSuccess:
-      return {
+      return adapter.addAll(action.payload.available, {
         ...state,
-        open_modal: false,
-        available: action.payload.available
-      };
+        open_modal: false
+      });
+    // return adapter.addAll(...state,  ) {
+    //   ...state,
+    //   open_modal: false,
+    //   available: action.payload.available
+    // };
     case ReserveActionTypes.ReserveRequested:
       return { ...state, confirm_reserve_modal: true };
     case ReserveActionTypes.ReserveConfirm:
+      return { ...state, confirm_reserve_modal: false };
+    case ReserveActionTypes.ConfirmReserveClosed:
       return { ...state, confirm_reserve_modal: false };
     default:
       return state;
--- src/app/user/user.selectors.ts
+++ src/app/user/user.selectors.ts
@@ -6,13 +6,24 @@ export const reserveState = createFeatureSelector<ReserveState>('reserveState');
 export const selectReserveModalOpen = createSelector(
   reserveState,
   reserve => {
+    console.log('selectReserveModalOpen');
     return reserve.open_modal;
   }
 );
 
-export const selectReserveRequest = createSelector(
+export const selectReserveRequestssss = createSelector(
   reserveState,
   reserve => {
+    console.log('selectReserveRequest');
+    return reserve.confirm_reserve_modal;
+  }
+);
+
+
+export const selectReserveModalOpen2 = createSelector(
+  reserveState,
+  reserve => {
+    console.log('selectReserveModalOpen');
     return reserve.confirm_reserve_modal;
   }
 );
